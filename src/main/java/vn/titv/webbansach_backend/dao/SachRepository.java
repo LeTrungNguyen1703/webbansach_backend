e321d820b0b2301f048c77e23be5908371766de4
Le Trung Nguyen <nguyenc2300110@student.ctu.edu.vn>
2024-12-23 21:39:20 +0700

    Them endpoint tai SachRepository

--- src/main/java/vn/titv/webbansach_backend/dao/SachRepository.java
+++ src/main/java/vn/titv/webbansach_backend/dao/SachRepository.java
@@ -1,10 +1,14 @@
 package vn.titv.webbansach_backend.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 import vn.titv.webbansach_backend.entity.Sach;
 
 @RepositoryRestResource(path = "sach")
 public interface SachRepository extends JpaRepository<Sach, Integer> {
+    Page<Sach> findByTenSachContaining(@RequestParam("tenSach") String tenSach, Pageable pageable);
 }
